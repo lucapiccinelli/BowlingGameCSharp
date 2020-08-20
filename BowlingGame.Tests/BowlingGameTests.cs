eee83bd43b7c9ff9c363b29a5f0fd5df90c822b9
Luca Piccinelli <luca.picci@gmail.com>
2020-08-20 09:37:13 +0200

    test list

---
+++ BowlingGame.Tests/BowlingGameTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace BowlingGame.Tests
+{
+    public class BowlingGameTests
+    {
+        // TODO: Show the total score on the last line. Update the total score while running. When a frame is incomplete it sums its current value.
+        // TODO: Show the score by frame on the first line. Update the score only when the frame is closed. Otherwise show a "-".
+
+        // One zero
+        // all zeros
+        // One zero frame
+        // Open frame
+        // all open frame
+        // spare
+        // all spare
+        // strike
+        // all strikes
+        // last frame
+
+        [Fact]
+        public void GIVEN_AnInputFromTheCommandLine_WHEN_ItIsAListOfRolls_THEN_ItPrintsTheTotalScoreOnANewLine()
+        {
+            
+        }
+    }
+}
