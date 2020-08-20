bcb7319048818b6f127b44813851759ca3c364c3
Luca Piccinelli <luca.picci@gmail.com>
2020-08-20 09:53:22 +0200

    roll refactoring

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -7,8 +7,27 @@ namespace BowlingGame
     {
         public static void Main(string[] args)
         {
-            int[] rolls = args.Select(int.Parse).ToArray();
-            Console.WriteLine(rolls.Sum());
+            Roll[] rolls = ParseInput(args);
+            int totalScore = ComputeTotalScore(rolls);
+            PrintTotalScore(totalScore);
+        }
+
+        private static void PrintTotalScore(int totalScore)
+        {
+            Console.WriteLine(totalScore);
+        }
+
+        private static int ComputeTotalScore(Roll[] rolls)
+        {
+            return rolls.Sum(roll => roll.Value);
+        }
+
+        private static Roll[] ParseInput(string[] args)
+        {
+            return args
+                .Select(int.Parse)
+                .Select(i => new Roll(i))
+                .ToArray();
         }
     }
 }
