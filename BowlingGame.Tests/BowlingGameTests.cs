7df8ddc389374f8f96d0f21c41529f63910ef068
Luca Piccinelli <luca.picci@gmail.com>
2020-09-24 16:45:30 +0200

    random case test

--- BowlingGame.Tests/BowlingGameTests.cs
+++ BowlingGame.Tests/BowlingGameTests.cs
@@ -6,11 +6,7 @@ namespace BowlingGame.Tests
 {
     public class BowlingGameTests
     {
-        // TODO: Show the total score on the last line. Update the total score while running. When a frame is incomplete it sums its current value.
         // TODO: Show the score by frame on the first line. Update the score only when the frame is closed. Otherwise show a "-".
-
-        // all strikes
-        // last frame
 
         [Theory]
         [InlineData("0", "0")]
@@ -24,7 +20,8 @@ namespace BowlingGame.Tests
         [InlineData("0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0", "0")]
         [InlineData("1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "20")]
         [InlineData("5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5", "150")]
-        [InlineData("10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10", "300")]
+        [InlineData("10,10,10,10,10,10,10,10,10,10,10,10", "300")]
+        [InlineData("10,9,1,5,5,7,2,10,10,10,9,0,8,2,9,1,10", "187")]
         public void GIVEN_AnInputFromTheCommandLine_WHEN_ItIsAListOfRolls_THEN_ItPrintsTheTotalScoreOnANewLine(string inputRollsStr, string expectedTotalScore)
         {
             using var myOut = new StringWriter();
